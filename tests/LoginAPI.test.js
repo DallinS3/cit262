2c88fc9555fce5b7aacb464ebb2c9271a260170c
Dal <dallinscott743@gmail.com>
2023-01-19 10:43:19 -0700

    api_test_pass_and_fail

---
+++ tests/LoginAPI.test.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+// async = allows us to read from the DB by moving on and coming back
+it ("Should get a login token", async () => {
+
+    // await fetch allows it to move 
+    const loginResponse = await fetch('https://dev.stedi.me/login', {
+
+    //method = POST says to give information to server
+    method: 'POST',
+    headers:{
+        'Content-Type': 'application/text'
+    },
+
+    // stringify = makes the following into JSON text to be read by the server
+    body:JSON.stringify({
+        "userName": "sco17014@byui.edu",
+        "password": "P@ssw0rd"
+    })
+})
+
+// 200 means authorized
+expect(loginResponse.status).toBe(200);
+
+});
